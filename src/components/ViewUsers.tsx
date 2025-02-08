2d592b85e5a2ef5527faed223d9919a66078240b
camilo9090 <camilou9090@gmail.com>
2025-02-08 12:33:49 -0500

    Agregar iconos de eliminación y filtrar usuarios activos en la vista de usuarios

--- src/components/ViewUsers.tsx
+++ src/components/ViewUsers.tsx
@@ -1,8 +1,11 @@
+import { useMemo } from "react"
 import { usersStore } from "../store"
+import { TrashIcon } from "@heroicons/react/24/solid";
 
 export default function viewUsers() {
 
     const usuarios = usersStore(state => state.listUser)
+    const usuariosFilters = useMemo(() => usuarios.filter(salida => salida.status === true), [usuarios])
 
     return (
 
@@ -14,16 +17,17 @@ export default function viewUsers() {
                         <th className="border p-2">Nombre</th>
                         <th className="border p-2">Apellido</th>
                         <th className="border p-2">Correo</th>
+                        <th className="border p-2">Acciones</th>
 
                     </tr>
                 </thead>
 
                 <tbody>
 
-                    {usuarios.map((users) => (
+                    {usuariosFilters.map((users) => (
 
                         <tr
-                            className="hover:bg-gray-400"
+                            className=""
                             key={users.id}>
 
 
@@ -31,6 +35,22 @@ export default function viewUsers() {
                             <td className="border p-2 text-center">{users.lastName}</td>
                             <td className="border p-2 text-center">{users.email}</td>
 
+                            <td className="border p-2 text-center">
+
+                                <div className="">
+                                 
+                                      <button className="bg-red-500 flex flex-row text-white p-2 rounded-lg w-full hover:bg-red-600 transition-colors">
+                                      <TrashIcon
+                                        width={20}
+                                      
+                                        />
+                                        Eliminar
+                                       </button>
+                                        
+                                </div>
+                                
+
+                            </td>
 
                         </tr>
 
